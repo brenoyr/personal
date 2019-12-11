455fd4e2965dd6c8fd1a558d12382c53f7abc92d
brenoyr <breno_yr@hotmail.com>
2019-12-10 21:39:58 -0600

    updated test.cpp

--- test.cpp
+++ test.cpp
@@ -3,15 +3,30 @@
  * Class:	CSC330 Programming Languages	*
  * Assignment:	Investigations			*
  * Instructor:	Dr. Gourd			*
- * Due Date:	December 16, 2019 		*
+ * Due Date:	December 18, 2019 		*
  ************************************************/
 
 #include <iostream>
 #include <iomanip>
 using namespace std;
 
+void testByRef(int arr[])
+{
+	arr[0] = 10;
+
+	cout << "changed value in arr[0] inside testByRef: " <<  arr[0] << endl;
+}
+
+void testByVal(int *arr)
+{
+	arr[1] = 20;
+
+	cout << "changed value in arr[1] inside testByVal: " << arr[1] << endl;
+}
+
 int main()
 {
+	// Investigation 1:
 	cout << endl << setfill('*') << setw(64) << '*'
 		<< "\n\t\tInvestigation 1: Goto Statement\n"
 		<< setw(64) << '*' << endl << endl;
@@ -25,15 +40,16 @@ int main()
 
 	int times = 1;
 
-	goto label;
+	goto label;	// executes 'label.' 
 
 	label:
 		cout << "Executing 'label' " << times << " time(s)\n";
 		times++;
 		if (times < 6)
-			goto label;
+			goto label; // loops back to label until times >= 6
 		cout << "\n\n";
 	
+	// Investigation 2:
 	cout << setw(64) << '*'
 		<< "\n\t\tInvestigation 2: Blocks\n"
 		<< setw(64) << '*' << endl << endl;
@@ -45,15 +61,19 @@ int main()
 		<< "between blocks' scopes\n\n"
 		<< "Examples:\n\n";
 
+	// Outer Block:
 	{
 		cout << "Entering outer block...\n\n";
 
-		int outerVar = 0;
+		// "test" will test variables declared 
+		// with the same name but in different scopes:
 		int test = 25;
+		int outerVar = 0;
 
 		cout << "outer variable: " << outerVar << endl
 			<< "locally declared test variable: " << test << "\n\n";
 
+		// Inner Block:
 		{
 			cout << "Entering inner block...\n\n";
 
@@ -65,6 +85,7 @@ int main()
 			       	<< "locally declared test variable: "
 				<< test << " (different than outer block)\n\n";
 			
+			// Loop Block:
 			cout << "Entering loop block:...\n\n";
 			for (int c = 1; c < 6; c++)
 			{
@@ -76,10 +97,30 @@ int main()
 		}
 	}
 
+	// Investigation 3:
 	cout << setw(64) << '*'
 		<< "\n\t\tInvestigation 3: |A|r|r|a|y|s|\n"
 		<< setw(64) << '*' << endl << endl;
 
+	int arr[5] = {1,2,3,4,5};
+
+	cout << "value of arr[0] before testByRef: " << arr[0] << endl;
+
+	testByRef(arr);
+
+	cout << "value of arr[0] after testByRef was called: " << arr[0] << endl << endl;
+
+	cout << "Therefore, this array in C++ was passed by reference.\n"
+		<< "\"arr\" was passed as a parameter for testByRef(),\n"
+		<< "and once testByRef() modified a value in \"arr,\" it\n"
+		<< "kept the modified value when it returned from testByRef()\n"
+	        << "and was back at main's scope." << endl << endl;
+
+	cout << "value of arr[1] before testByVal is called: " << arr[1] << endl;
+
+	testByVal(arr);
+
+	cout << "value of arr[1] after testByVal was called: " << arr[1] << endl;
 
 	return 0;
 }
