5f632d8f86683914add15a3cae2c96a6ad9a1b61
brenoyr <43651439+brenoyr@users.noreply.github.com>
2020-02-27 17:17:58 -0600

    Upload Pascal0 Fibonacci sequence

--- Pascal0 Compilers/Compiler 4/fibonacci.pas
+++ Pascal0 Compilers/Compiler 4/fibonacci.pas
@@ -1,18 +1,18 @@
-VAR F;
+VAR A;
 
 FUNCTION FIBONACCI(VAL N);
     BEGIN
-        IF (N <= 2) THEN
+        IF (N < 2) THEN
             IF (N = 0) THEN
                 FIBONACCI := 0
             ELSE
                 FIBONACCI := 1
         ELSE
-            FIBONACCI := CALL FIBONACCI(N - 1) + CALL FIBONACCI(N -2);
+            FIBONACCI := CALL FIBONACCI(N - 1) + CALL FIBONACCI(N - 2);
     END;
 
 BEGIN
-    FOR F := 1 TO 19 DO
-        WRITE(CALL FIBONACCI(F));
-    WRITELN(CALL FIBONACCI(F));
+    FOR A := 1 TO 19 DO
+        WRITE(CALL FIBONACCI(A));
+    WRITELN(CALL FIBONACCI(A));
 END.
