f1ed607fbf8a1452c84c842afde7c6c2d759c1ec
brenoyr <43651439+brenoyr@users.noreply.github.com>
2020-03-18 16:35:20 -0500

    Cleaned up output, have to fix threshold

--- Applied Cryptography/brute/et-tu-brute.py
+++ Applied Cryptography/brute/et-tu-brute.py
@@ -20,23 +20,23 @@ DICTIONARY_FILE = "dictionary.txt"
 # performs a ROT
 def rot(ciphertext, shift):
     rotatedcipher = ""
+
     for line in ciphertext:                                 # get a line in the ciphertext
         cipherline = list(line)                             # work with that line as a list
+
         for i in range (0, len(cipherline)):                # loop through every character
-            alphabetIndex = ALPHABET.find(cipherline[i])
-            rotIndex = alphabetIndex + shift
-            if (rotIndex >= len(ALPHABET)):
-                rotIndex -= len(ALPHABET)
-            #print i, " AND ALPHA: ", rotIndex
-            cipherline[i] = ALPHABET[rotIndex]
+            alphabetIndex = ALPHABET.find(cipherline[i])    # find the index of the current letter in our alphabet
+            rotIndex = alphabetIndex + shift                # rotation index the current letter's index + shift
+
+            if (rotIndex >= len(ALPHABET)):                 # if rotIndex is bigger than alphabet's length, just
+                rotIndex -= len(ALPHABET)                   # subtract to loop back to the beginning of the alphabet
+            
+            cipherline[i] = ALPHABET[rotIndex]              # substitute letter for the shifted letter
 
         rotatedcipher += "".join(cipherline)
-        print rotatedcipher
-        
-
-        #print cipherline
-    # print rotatedcipher
-    pass
+        rotatedcipher += "\n"
+    
+    return rotatedcipher
 
 # read in the dictonary:
 f = open(DICTIONARY_FILE, "r")
@@ -49,15 +49,25 @@ ciphertext = ciphertext.split("\n")[:10]    #################### dont forget to 
 
 # apply rotations through the entire alphabet:
 for i in range (1, len(ALPHABET)):
-    plaintext = rot(ciphertext, i)
-#     words = plaintext.split(" ")
-
-#     # checking if the words in the plaintext match words in the dictionary:
-#     count = 0
-#     for word in words:
-#         if word in dictionary:
-#             count += 1
     
-#     # if enough words match word in the dictionary:
-#     if (count > len(words) * THRESHOLD):
-#         print "Shift is {}\nPlaintext: {}".format(95-i, words)
+    # print "\n\n", i
+    plaintext = rot(ciphertext, i)
+    words = plaintext.split(" ")
+
+    # checking if the words in the plaintext match words in the dictionary:
+    count = 0
+    for word in words:
+        validWord = ""
+
+        for w in word:
+            if w.isalpha():
+                validWord = "".join([validWord, w]).lower()
+        #print validWord
+        if validWord in dictionary.lower():
+            count += 1
+
+    # print count, i
+    # if enough words match word in the dictionary:
+    if (count > len(words) * THRESHOLD):
+        print "Shift is {}\nPlaintext: {}".format(95-i, plaintext)
+        # print "Shift is {}\nPlaintext: {}".format(95-i, "".join(words))
