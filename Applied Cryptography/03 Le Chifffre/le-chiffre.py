15f77f8cb8b9105f240aa64d1cc30f7e7afc368a
brenoyr <breno_yr@hotmail.com>
2020-04-10 03:47:13 -0500

    Implemented efficiency algorithm for long ciphertexts

--- Applied Cryptography/03 Le Chifffre/le-chiffre.py
+++ Applied Cryptography/03 Le Chifffre/le-chiffre.py
@@ -4,6 +4,11 @@
 #   A:          Le Chiffre                              #
 #   Due Date:   04/17/2020                              #
 #   Comments:   Python 2.7.17                           #
+#                                                       #
+#               Works with ciphertexts 1-4.             #
+#                                                       #
+#               I'm happy with the results of the       #
+#               efficiency method/check I implemented   #
 #########################################################
 
 from sys import stdin
@@ -11,7 +16,7 @@ import re
 
 # the alphabet
 ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789`~!@#$%^&*()-_=+[{]}\|;:'\",<.>/? "
-ALPHABET = " -,;:!?/.'\"()[]$&#%012345789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxyYzZ"
+# ALPHABET = " -,;:!?/.'\"()[]$&#%012345789aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxyYzZ"
 
 # the threshold and minimum length for a word to be considered "valid"
 # 0.5 & 2 works for the assignment ciphertexts
@@ -51,11 +56,11 @@ def originalText(ciphertext, keyString):
         for i in range (0, len(cipherLine)):                        # loop through every character
             # find the index in the original alphabet:
             # original index = index in old/ciphered text - index in key:
-            cipher_index = ALPHABET.find(cipherLine[i])             # index in old/ciphered text
-            key_index = ALPHABET.find(keyString[keyTextIndex])      # index in key
+            cipherIndex = ALPHABET.find(cipherLine[i])              # index in old/ciphered text
+            keyIndex = ALPHABET.find(keyString[keyTextIndex])       # index in key
             keyTextIndex += 1                                       # IMPORTANT: update current index in our keyString
-            plain_index = cipher_index - key_index
-            plaintext += ALPHABET[plain_index % len(ALPHABET)]      # append original/plain char, which is gotten with
+            plainIndex = cipherIndex - keyIndex
+            plaintext += ALPHABET[plainIndex % len(ALPHABET)]       # append original/plain char, which is gotten with
                                                                     # mod len(ALPHABET) to keep our index within the alphabet
 
         plaintext += "\n"                                           # done with a line, append "\n" and go to the next one
@@ -73,6 +78,19 @@ f.close()
 # dictionary = [ re.sub(r"[^\w]+", "", x) for x in dictionary ]
 
 ciphertext = stdin.read().rstrip("\n")
+
+# EFFICIENCY CHECK:
+# just work with the first 10 lines.
+# If there are more, only generate the full
+# plaintext if the first 10 passed the test.
+# Set a flag to remember it's a long ciphertext,
+# and save original ciphertext before we split it
+# into only the first 10 lines:
+LONG = False
+if len(ciphertext.split("\n")) >= 10:
+    longCipherText = ciphertext
+    LONG = True
+
 ciphertext = "\n".join(ciphertext.split("\n")[:10])
 
 # checking every word in the dictionary for a possible valid key:
@@ -83,7 +101,7 @@ for keyword in dictionary:
         # regex that deletes any (greedy) char that is not a-z or A-Z
         # note: using either r"[^a-zA-z]+" or r"[^\w]+" works for this assignment
         # NOTE TO SELF: maybe r"[^\w]+" will work better for the challenge so use that as default
-        plain_keyword = re.sub(r'[^\w]','', keyword)
+        plainKeyword = re.sub(r'[^\w]','', keyword)
 
     plaintext = originalText(ciphertext, key)
     # checking if the words in the plaintext match words in the dictionary
@@ -95,5 +113,14 @@ for keyword in dictionary:
             count += 1
 
     # if enough words match word in the dictionary:
-    if count >  len(words) * THRESHOLD:
+    if count > len(words) * THRESHOLD:
+        
+        # generate full key string and full plaintext
+        # if there are more than 10 lines and
+        # the first 10 passed the test:
+        if (LONG):
+            key = generateKey(longCipherText, keyword)
+            longCipherText = "\n".join(longCipherText.split("\n"))
+            plaintext = originalText(longCipherText, key)
+
         print "KEY={}:\n{}".format(keyword, plaintext)
