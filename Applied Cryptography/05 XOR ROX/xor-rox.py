aa0a5530cf02604a8b652ed2570801236c359dc4
brenoyr <breno_yr@hotmail.com>
2020-05-05 03:10:22 -0500

    Final version of XOR ROX

--- Applied Cryptography/05 XOR ROX/xor-rox.py
+++ Applied Cryptography/05 XOR ROX/xor-rox.py
@@ -4,6 +4,9 @@
 #   Due Date:   05/05/2020                              #
 #   Comments:   Python 2.7.17                           #
 #                                                       #
+#               Encrypt and Decrypt working             #
+#                                                       #
+#             !!  PLEASE SAVE KEY AS "05.key" !!        #
 #########################################################
 
 # XOR ROX
@@ -15,63 +18,121 @@ from PIL import Image
 
 from random import randint
 
-RGB_RANGE = 256
+# mode
+DECRYPT = False
+# DECRYPT = True
 
 # the images
 INPUT_IMAGE = "input.png"
 AND_IMAGE = "and.png"
 OR_IMAGE = "or.png"
 XOR_IMAGE = "xor.png"
+DECRYPTED_IMAGE = "decrypted.png"
 
-# get the input image
-img_for_and = Image.open(INPUT_IMAGE)
-img_for_or = Image.open(INPUT_IMAGE)
-img_for_xor = Image.open(INPUT_IMAGE)
+# Encryption
+if (DECRYPT == False):
+    # get the input image
+    img_for_and = Image.open(INPUT_IMAGE)
+    img_for_or = Image.open(INPUT_IMAGE)
+    img_for_xor = Image.open(INPUT_IMAGE)
 
-pixels_and = img_for_and.load()
-pixels_or = img_for_or.load()
-pixels_xor = img_for_xor.load()
+    # load pixels for each
+    pixels_and = img_for_and.load()
+    pixels_or = img_for_or.load()
+    pixels_xor = img_for_xor.load()
 
-rows, cols = img_for_and.size
+    # currently they are all the same so
+    # we can get rows and cols from any of them
+    rows, cols = img_for_xor.size
 
+    # go through every pixel
+    for row in range(0, rows):
+        for col in range(0, cols):
+            # get the current pixel
+            # all 3 are still the same so get rgb from any
+            r, g, b = pixels_xor[row, col]
 
-# pick some pixels to change
-for row in range(0, rows):
-    for col in range(0, cols):
-        # get the current pixel
-        r, g, b = pixels_and[row, col]
+            # generate and display the key pixel RGB values
+            key_r = randint(0, 255)
+            key_g = randint(0, 255)
+            key_b = randint(0, 255)
+            print "{},{},{}".format(key_r, key_g, key_b)
 
-        # generate and display the key pixel RGB values
-        key_r = randint(0, 255)
-        key_g = randint(0, 255)
-        key_b = randint(0, 255)
-        print "{},{},{}".format(key_r, key_g, key_b)
+            # change the current pixel RGB values for AND
+            and_r = r & key_r
+            and_g = g & key_g
+            and_b = b & key_b
 
-        # change the current pixel RGB values for AND
-        and_r = r & key_r
-        and_g = g & key_g
-        and_b = b & key_b
+            # change the current pixel RGB values for OR
+            or_r = r | key_r
+            or_g = g | key_g
+            or_b = b | key_b
 
-        pixels_and[row, col] = (and_r, and_g, and_b)
+            # change the current pixel RGB values for XOR
+            xor_r = r ^ key_r
+            xor_g = g ^ key_g
+            xor_b = b ^ key_b
 
-        # change the current pixel RGB values for OR
-        or_r = r | key_r
-        or_g = g | key_g
-        or_b = b | key_b
+            # overwrite pixels with the new rgb values
+            pixels_and[row, col] = (and_r, and_g, and_b)
+            pixels_or[row, col] = (or_r, or_g, or_b)
+            pixels_xor[row, col] = (xor_r, xor_g, xor_b)
 
-        pixels_or[row, col] = (or_r, or_g, or_b)
-        
+    # write the new image
+    img_for_and.save(AND_IMAGE)
+    img_for_or.save(OR_IMAGE)
+    img_for_xor.save(XOR_IMAGE)
+    exit(0)
 
-        # change the current pixel RGB values for XOR
-        xor_r = r ^ key_r
-        xor_g = g ^ key_g
-        xor_b = b ^ key_b
+# Decryption
+else:
+    # read the key file
+    KEY_FILE = "05.key"
 
-        pixels_xor[row, col] = (xor_r, xor_g, xor_b)
+    f = open(KEY_FILE, "r")
+    key = f.read().rstrip("\n")
+    f.close()
 
+    # make it into a list of rgb values
+    key = key.split("\n")
 
+    # get the xor'ed image
+    img = Image.open("xor.png")
 
-# write the new image
-img_for_and.save(AND_IMAGE)
-img_for_or.save(OR_IMAGE)
-img_for_xor.save(XOR_IMAGE)
+    # load pixels
+    pixels = img.load()
+
+    # get it's rows and cols numbers
+    rows, cols = img.size
+    
+    # counter to loop through the list of key values
+    i = 0
+
+    # pick some pixels to change
+    for row in range(0, rows):
+        for col in range(0, cols):
+            # get the current pixel's rgb values
+            xor_r, xor_g, xor_b = pixels[row, col]
+
+            # get the key used to encrypt
+            key_r, key_g, key_b = key[i].split(',')
+
+            # str -> int
+            key_r = int(key_r)
+            key_g = int(key_g)
+            key_b = int(key_b)
+            
+            # increment counter to go to the next key values in the next loop
+            i += 1
+
+            # recover the original rgb values
+            r = xor_r ^ key_r
+            g = xor_g ^ key_g
+            b = xor_b ^ key_b
+
+            # overwrite pixels with the original rgb values
+            pixels[row, col] = (r, g, b)
+    
+    # write the new decrypted image
+    img.save(DECRYPTED_IMAGE)
+    exit(0)
